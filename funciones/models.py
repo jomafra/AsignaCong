ad3533043f3c702106a47f2c8567e222f0bf4de5
josé maría <jose-fragozo@hotmail.com>
2022-08-30 20:13:52 -0500

    formateo de fecha

--- funciones/models.py
+++ funciones/models.py
@@ -56,11 +56,10 @@ class Plataforma (models.Model):
     
     class Meta:
         ordering = ['fecha','voluntario_Id']
-    #formatedfecha =self.fecha.strftime('%d-%m-%Y')
 
     def __str__(self):
         txt ="{0} {1}"
-        return txt.format(self.fecha.strftime('%d-%m-%Y'), self.voluntario_Id)
+        return txt.format(self.fecha, self.voluntario_Id)
 
 
 
